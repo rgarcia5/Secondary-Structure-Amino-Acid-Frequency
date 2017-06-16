0df65fefff5b53025de90907cc0d41d868463c16
Raymond Garcia <raymond_jay_garcia@yahoo.com>
2017-06-16 15:41:48 -0700

    Delete exercise7.py

--- exercise7.py
+++
@@ -1,9 +0,0 @@
-def total_co2_generated():
-  data = ((10, 58.2),(20, 65.2),(30, 67.8), (40, 65.4), (50, 58.8), (60, 49.6), (70, 39.1), (80, 15.8))
-  total = 0
-  for datum in data:
-    total += (datum[1] * 10)
-  return total
-
-
-print (total_co2_generated())
